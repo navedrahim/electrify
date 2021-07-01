e941de6468aa684f820150c9483c18676b7e0bbc
navedrahim <nrah1988@gmail.com>
2021-06-30 22:30:16 -0400

    finished form

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -3,9 +3,9 @@ import { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import { baseURL, config } from "../services";
 
-function AddCar(props) {
+function Form(props) {
   const [make, setMake] = useState("");
-  const [model, setModel]= useState("");
+  const [model, setModel] = useState("");
   const [minRange, setMinRange] = useState(0);
   const [maxRange, setMaxRange] = useState(0);
   const [price, setPrice] = useState(0);
@@ -23,7 +23,7 @@ function AddCar(props) {
         setPrice(editCar.fields.price);
       }
     }
-  }, [params.id, props.cars])
+  }, [params.id, props.cars]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -44,12 +44,53 @@ function AddCar(props) {
     setTimeout(() => {
       history.push("/");
     }, 1000);
-  }
+  };
 
   return (
-    <form onSubmit
-
-  )
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="make">Make:</label>
+      <input
+        id="make"
+        type="text"
+        onChange={(e) => setMake(e.target.value)}
+        value={make}
+        required
+      />
+      <label htmlFor="model">Model:</label>
+      <input
+        id="model"
+        type="text"
+        onChange={(e) => setModel(e.target.value)}
+        value={model}
+        required
+      />
+      <label htmlFor="minRange">Minimum Range:</label>
+      <input
+        id="minRange"
+        type="number"
+        onChange={(e) => setMinRange(e.target.valueAsNumber)}
+        value={minRange}
+        required
+      />
+      <label htmlFor="maxRange">Maximum Range:</label>
+      <input
+        id="maxRange"
+        type="number"
+        onChange={(e) => setMaxRange(e.target.valueAsNumber)}
+        value={maxRange}
+        required
+      />
+      <label htmlFor="price">Price: $</label>
+      <input
+        id="price"
+        type="number"
+        onChange={(e) => setPrice(e.target.valueAsNumber)}
+        value={price}
+        required
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default Form;
